a966ba4a4143cdc07e966cbaa8ea74063e368faf
Lizbeth Alcaraz <ldalcara@uci.edu>
2022-11-15 22:08:07 -0800

    SpawnPoint upgrade

--- Assets/Scripts/TriggerSpawnPoint.cs
+++ Assets/Scripts/TriggerSpawnPoint.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class TriggerSpawnPoint : MonoBehaviour
 {
+    [SerializeField] private string objTag;
 
+    // When gameobject is on moving object, attach to it by making them
+    // a child when triggered.
+    private void OnTriggerEnter(Collider other)
+    {
+        // if player
+        if (other.gameObject.tag == objTag)
+        {
+            Player player = other.gameObject.GetComponent<Player>();
+            player.UpdateSpawn(this.transform);
+        }
+    }
 }
