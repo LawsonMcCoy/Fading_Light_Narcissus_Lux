40aab1ec718b7ea3fcfb2990d6d218f89a7068ea
Lizbeth Alcaraz <ldalcara@uci.edu>
2023-01-16 20:24:18 -0800

    Created Fancy Spawn Point

--- Assets/Scripts/TriggerSpawnPoint.cs
+++ Assets/Scripts/TriggerSpawnPoint.cs
@@ -5,9 +5,29 @@ using UnityEngine;
 public class TriggerSpawnPoint : MonoBehaviour
 {
     [SerializeField] private string objTag;
+    [SerializeField] private ParticleSystem particle;
+    [SerializeField] private string hexColor = "#32a852";
+    [SerializeField] private float newSimulationSpeed = 0.4f;
 
-    // When gameobject is on moving object, attach to it by making them
-    // a child when triggered.
+    private ParticleSystem.MainModule main;
+    private float originalSpeed;
+    private ParticleSystem.MinMaxGradient originalColor;
+    private Color newColor;
+
+    private void Start()
+    {
+        main = particle.main;
+        originalSpeed = main.simulationSpeed;
+        originalColor = main.startColor;
+
+        if (!hexColor.Contains("#"))
+        {
+           hexColor = "#" + hexColor; // add # in front of hexcode
+        }
+
+        ColorUtility.TryParseHtmlString(hexColor, out newColor);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         // if player
@@ -15,6 +35,19 @@ public class TriggerSpawnPoint : MonoBehaviour
         {
             Player player = other.gameObject.GetComponent<Player>();
             player.UpdateSpawn(this.transform);
+
+            main.startColor = newColor;
+            main.simulationSpeed = newSimulationSpeed;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // if player
+        if (other.gameObject.tag == objTag)
+        {
+            main.startColor = originalColor;
+            main.simulationSpeed = originalSpeed;
         }
     }
 }
