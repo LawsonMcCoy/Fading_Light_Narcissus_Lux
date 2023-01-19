466070ca77f42202c60af0c737ab2bee8c11a37d
Marc Hagoriles <markyboyph@gmail.com>
2023-01-18 16:31:09 -0800

    Added background sprite for Quest UI
    -Added background sprite for Quest UI

---
+++ Assets/QuestHUD.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class QuestHUD : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI questText;
+
+    private void Update()
+    {
+        
+    }
+}
