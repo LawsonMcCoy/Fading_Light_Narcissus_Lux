31a204f48aa154bf39fac364d7a0daa2aef851ad
Marc Hagoriles <markyboyph@gmail.com>
2023-03-16 15:47:02 -0700

    Added healthbar, stamina, and fixed bug

--- Assets/Scripts/Entities/PlayerHealth.cs
+++ Assets/Scripts/Entities/PlayerHealth.cs
@@ -2,9 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class PlayerHealth : HealthManager
 {
+    [SerializeField] private Sprite[] healthbarSprites; //A list of all the different sprites, partitioned by 20
+    [SerializeField] private GameObject healthBar; //Reference to the healthbar object.
+    private int healthBarIndex = 0;
+
+    //For the shaking animation
+    [SerializeField] private float shakeDuration = 0.7f;
+    [SerializeField] private float shakeDistance = 5f;
+    private Vector3 startPos;
+
+    //For the changing of colors
+    private float lerpValue = 0f;
+
+    private void Start()
+    {
+        startPos = healthBar.transform.position;
+    }
+
+
+    public override void Subtract(float damage)
+    {
+        base.Subtract(damage);
+
+        beginShakeHealthBarCoroutine();
+
+        //Partitioned by 20 (since there are 5 leaves)
+        if ((base.maxResource - base.resource) % (base.maxResource / 5) == 0)
+        {
+            healthBarIndex++;
+
+            healthBar.GetComponent<Image>().sprite = healthbarSprites[healthBarIndex];
+        }
+    }
+
     // Start is called before the first frame update
     protected override void OnDeath()
     {
@@ -12,7 +46,46 @@ public class PlayerHealth : HealthManager
 
         //gameObject.transform.position = saveData.spawnPoint;
         Add(1000.0f);
+        healthBarIndex = 0; //Reset healthbar index
+        healthBar.GetComponent<Image>().sprite = healthbarSprites[healthBarIndex]; //Update it back to full health sprite.
         EventManager.Instance.Notify(EventTypes.Events.PLAYER_DEATH);
 
     }
+
+    public void beginShakeHealthBarCoroutine()
+    {
+        Debug.Log("Shaking now");
+        StartCoroutine(ShakeHealthbar());
+    }
+
+    private IEnumerator ShakeHealthbar()
+    {
+        //Vector3 startPos = healthBar.transform.position;
+        Color origColor = healthBar.GetComponent<Image>().color;
+
+        float timer = 0f;
+        while (timer < shakeDuration)
+        {
+            timer += Time.deltaTime;
+
+            lerpValue += Time.deltaTime / shakeDuration;
+            healthBar.GetComponent<Image>().color = Color.Lerp(healthBar.GetComponent<Image>().color, Color.red, lerpValue);
+
+            Vector3 newPos = startPos + (Random.insideUnitSphere * shakeDistance);
+            //newPos.y = healthBar.transform.position.y;
+            //newPos.z = healthBar.transform.position.z;
+
+            healthBar.transform.position = newPos;
+
+            healthBar.GetComponent<Image>().color = Color.Lerp(healthBar.GetComponent<Image>().color, origColor, lerpValue);
+
+            yield return null;
+
+        }
+
+        healthBar.transform.position = startPos;
+        lerpValue = 0f;
+    }
+
+
 }
